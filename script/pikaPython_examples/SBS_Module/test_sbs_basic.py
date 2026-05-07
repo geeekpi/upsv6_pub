932883f0b49cd5609c73e75112840fd49d2621a3
yoyojacky <yoyojacky2009@gmail.com>
2026-05-07 11:00:56 +0800

    update sbs test script
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

---
+++ script/pikaPython_examples/SBS_Module/test_sbs_basic.py
@@ -0,0 +1,29 @@
+from UPS import SBS, I2C, Device
+
+
+SBS(I2C, 0x0a)
+
+if not SBS.ping():
+    print("No SBS device found!")
+else:
+    while True:
+        print("\n--- Voltage ---")
+        v = SBS.voltageMv()
+        print("Voltage:", v, "mV")
+
+        print("--- Current ---")
+        c = SBS.currentMa()
+        print("Current:", c, "mA")
+        print("Discharging:", SBS.isDischarging())
+
+        print("--- SOC ---")
+        rsoc = SBS.relativeStateOfChargePercent()
+        print("RSOC:", rsoc, "%")
+
+        print("--- Temp ---")
+        t = SBS.temperatureC10()
+        print("Temp:", t/10.0, "C")
+
+        print("-" * 30)
+        Device.sleep(5000)
+

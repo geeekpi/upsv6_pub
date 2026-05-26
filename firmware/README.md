9161fe83783ec3efa61cad638490314a0fb27455
yoyojacky <yoyojacky2009@gmail.com>
2026-05-26 17:05:26 +0800

    update firmware to Rev1.2, adding SBS support
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- firmware/README.md
+++ firmware/README.md
@@ -20,6 +20,7 @@ rev1.0-efa805273c2e3dcc75291db4b8e685ad57a9b78d.bin
 
 - `rev1.0`: Basic Function and TFT LCD display extension board support.
 - `rev1.1`: Fixed the LED register's customized function and `SATA Power board` extension board support.
+- `rev1.2`: Adding SBS(smart battery system) expansion board support.
 
 ## Important Notes
 
--- script/pikaPython_examples/SBS_Module/test_sbs_basic.py
+++ script/pikaPython_examples/SBS_Module/test_sbs_basic.py
@@ -1,29 +1,38 @@
 from UPS import SBS, I2C, Device
 
 
-SBS(I2C, 0x0a)
+SBS.init(I2C, 0x0a)
 
 if not SBS.ping():
     print("No SBS device found!")
 else:
     while True:
-        print("\n--- Voltage ---")
+        addr = SBS.getAddr() 
+        print("Addr: ", addr)
         v = SBS.voltageMv()
+        Device.sleep(5000)
         print("Voltage:", v, "mV")
-
-        print("--- Current ---")
         c = SBS.currentMa()
         print("Current:", c, "mA")
-        print("Discharging:", SBS.isDischarging())
-
-        print("--- SOC ---")
+        Device.sleep(5000)
+        ac = SBS.averageCurrentMa()
+        print("Average Current:", ac, "mA")
+        Device.sleep(5000)
+        if SBS.isDischarging():
+            print("Discharging...")
+        else:
+            print("Charging....")
+        Device.sleep(5000)
         rsoc = SBS.relativeStateOfChargePercent()
         print("RSOC:", rsoc, "%")
-
-        print("--- Temp ---")
+        Device.sleep(5000)
+        ascp = SBS.absoluteStateOfChargePercent()
+        print("ASCP:", ascp, "%")
+        Device.sleep(5000)
         t = SBS.temperatureC10()
-        print("Temp:", t/10.0, "C")
-
-        print("-" * 30)
+        if t < 0:
+            t = SBS.temperatureC10()    
+        else:
+            print("Temp:", t/10.0, "C")
         Device.sleep(5000)
 
